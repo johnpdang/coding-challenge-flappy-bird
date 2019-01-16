814b5b5632b6ea61d3ee50bf993ad811b68fd0e2
john <johnpdang@gmail.com>
2019-01-16 15:21:16 -0800

    Add files via upload

---
+++ flappy-bird-clone/bird.js
@@ -0,0 +1,22 @@
+function Bird() {
+    this.y = height/2;
+    this.x = 64;
+
+    this.gravity = 1;
+    this.velocity = 0;
+
+    this.show = function() {
+        fill(255);
+        ellipse(this.x, this.y, 32, 32);
+    }
+
+    this.update() = function() {
+        this.velocity += this.gravity;
+        this. y +=velocity;
+    }
+
+    if (this. y > height) {
+        this.y = height;
+        this.velocity = 0;
+    }
+}
---
+++ flappy-bird-clone/sketch.js
@@ -0,0 +1,12 @@
+var bird;
+
+function setup() {
+    createCanvas(400, 600);
+    bird = new Bird();
+}
+
+function draw() {
+    background(0);
+    bird.update();
+    bird.show();
+}
